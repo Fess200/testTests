9a69895c03418068eb3f2f0d869a83d15dcf3513
Andrew Katyushin <fess200@yandex.ru>
2013-11-18 14:39:47 +0400

    Initial Commit

---
+++ testClick/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testClick
+//
+//  Created by Andrew on 18.11.13.
+//  Copyright (c) 2013 Irevolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ testClick/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  testClick
+//
+//  Created by Andrew on 18.11.13.
+//  Copyright (c) 2013 Irevolution. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
